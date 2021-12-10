79b556123547aa64948e0762950fe121854781e8
ObamaBolsa <daniel.alegria.lino@gmail.com>
2021-12-10 15:47:16 -0600

    First Commit

---
+++ Palindrome.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace Palindrome.Test
+{
+	public class UnitTest1
+	{
+		[Theory]
+		[InlineData()]
+		public void Test1()
+		{
+
+		}
+	}
+}
---
+++ PalindromeNumbers/Code/Main.cs
@@ -0,0 +1,15 @@
+﻿bool IsPalindrome(int a_number2check) 
+{
+	string number_in_letters = a_number2check.ToString();
+
+	for (int i=0; i<number_in_letters.Length/2 ; i++) 
+	{
+		Console.WriteLine(number_in_letters[i]);
+		Console.WriteLine(number_in_letters[number_in_letters.Length-(i+1)]);
+	}
+
+
+	return true;
+}
+
+Console.WriteLine( IsPalindrome (23246234) );
