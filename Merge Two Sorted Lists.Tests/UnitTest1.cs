a85f848533ccddb00a3678ce60a7f0dc2619ab4d
ObamaBolsa <daniel.alegria.lino@gmail.com>
2021-12-16 16:16:44 -0600

    Initializated Merge Two Sorted List Sln and c# Project

---
+++ Merge Two Sorted Lists.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Merge_Two_Sorted_Lists.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
---
+++ Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,35 @@
+﻿var sol = new Solution();
+var lista1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+var lista2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+
+sol.MergeTwoLists(lista1, lista2 );
+
+
+public class ListNode
+{
+	public int val;
+	public ListNode next;
+	public ListNode(int val = 0, ListNode next = null)
+	{
+		this.val = val;
+		this.next = next;
+	}
+}
+
+public class Solution
+{
+	public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+	{
+
+
+		var current_list_node = list1;
+		do
+		{
+			Console.WriteLine(current_list_node.val);
+			current_list_node = current_list_node.next;
+		} while (current_list_node != null);
+
+		return new ListNode();
+	}
+}
