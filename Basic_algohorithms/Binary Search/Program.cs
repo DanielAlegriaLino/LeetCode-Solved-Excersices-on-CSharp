5ae36d0960d4c377f0ace61d9b70334918e74566
ObamaBolsa <daniel.alegria.lino@gmail.com>
2021-12-25 22:19:31 -0600

    Binary search algohorithm implemented and tested

---
+++ Basic_algohorithms/Binary Search/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] sorted_array = new int[] { 0, 0, 0, 0, 0, 0, 1, 2, 2, 2 };
+
+public class Solution
+{
+	public static int binarySearch(int[] sorted_array, int target)
+	{
+		int first_pivot = 0;
+		int last_pivot = sorted_array.Length - 1;
+		int mid_pivot = (first_pivot + last_pivot) / 2;
+
+		int solution = 0;
+
+		while (first_pivot <= last_pivot)
+		{
+			mid_pivot = (first_pivot + last_pivot) / 2;
+
+			if(first_pivot == last_pivot) { return first_pivot; }
+			else if (sorted_array[mid_pivot] == target) { return mid_pivot; }
+			else if (sorted_array[mid_pivot] < target) { first_pivot = mid_pivot + 1; }
+			else if (sorted_array[mid_pivot] > target) { last_pivot = mid_pivot - 1; }
+		}
+
+		return -1;
+
+	}
+
+
+}
+
