6920b37f19d562965242cbff7a2c19dc9a290e0f
ObamaBolsa <daniel.alegria.lino@gmail.com>
2022-01-05 17:28:17 -0600

    Solved Build Array from Permutation

---
+++ Build Array from Permutation/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("");
+
+
+public class Solution
+{
+	public int[] BuildArray(int[] nums)
+	{
+		int[] result = new int[nums.Length];
+
+		for (int i = 0; i < result.Length; i++)
+		{
+			result[i] = nums[nums[i]];
+		}
+
+		return result;
+	}
+
+
+}
