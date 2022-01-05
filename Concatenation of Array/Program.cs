964366f0f9a0991be3cb420b91b53b8adf5b7c7c
ObamaBolsa <daniel.alegria.lino@gmail.com>
2022-01-05 17:56:01 -0600

    Concatenation of Array

---
+++ Concatenation of Array/Program.cs
@@ -0,0 +1,19 @@
+﻿// this code duplicate the full content of an array
+// Example [1,2,3] will turn into [1,2,3,1,2,3]
+Console.WriteLine("Solved");
+public class Solution
+{
+	public int[] GetConcatenation(int[] nums)
+	{
+		int duplicated_array_lenght = nums.Length * 2;
+		int[] duplicated_array = new int[duplicated_array_lenght];
+
+		for (int i = 0; i < nums.Length; i++)
+		{
+			duplicated_array[i] = nums[i];
+			duplicated_array[i + nums.Length] = nums[i];
+		}
+
+		return duplicated_array;
+	}
+}
