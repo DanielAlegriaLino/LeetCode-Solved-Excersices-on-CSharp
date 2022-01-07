927f5d85c0fe717b49d7259f6799017ade5f96c2
ObamaBolsa <daniel.alegria.lino@gmail.com>
2022-01-06 21:32:38 -0600

    Running Sum of 1d Array

---
+++ Running Sum of 1d Array/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("");
+
+public class Solution
+{
+  public int[] RunningSum(int[] nums)
+  {
+
+    int total_sum = 0;
+    for (int i = 0; i < nums.Length; i++) 
+    {
+      total_sum+= nums[i];
+      nums[i] = total_sum;
+    }
+
+    return nums; 
+
+  }
+}
